437d8d5fa1b0afb6f85fa92cd12f9806f2d25261
Manuela <manuela.belmino@academico.ifpb.edu.br>
2021-03-10 12:19:21 -0300

    sequelize

--- src/index.js
+++ src/index.js
@@ -10,13 +10,17 @@ async function inserir (obj) {
     console.log(res);
 }
 
-//let p1 = {nome: "Jack", sobrenome: "Sparrow", endereco: "Rua do Jack"};
-//let p2 = {nome: "Viola", sobrenome: "Willoughby", endereco: "Rua da Viola"};
-//let p3 = {nome: "Harry", sobrenome: "Potter", endereco: "Rua do Harry"};
+let p1 = {nome: "Jack", sobrenome: "Sparrow", endereco: "Rua do Jack"};
+let p2 = {nome: "Viola", sobrenome: "Willoughby", endereco: "Rua da Viola"};
+let p3 = {nome: "Harry", sobrenome: "Potter", endereco: "Rua do Harry"};
 let p4 = {nome: "Sakura", sobrenome: "Haruno", endereco: "Rua da Sakura"};
-//let p5 = {nome: "Leon", sobrenome: "Kennedy", endereco: "Rua do Leon"};
+let p5 = {nome: "Leon", sobrenome: "Kennedy", endereco: "Rua do Leon"};
 
+inserir(p1);
+inserir(p2);
+inserir(p3);
 inserir(p4);
+inserir(p5);
 
 async function consultar() {
     let r = await Pessoa.findAll();
